1a3a176b510ffdfe65e38421b1683f0f2138b579
Mohmmad Shah Faishal <faishal@scrambleapps.com>
2022-05-11 15:48:12 +0530

    Initial Commit

---
+++ Twitter Clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Twitter Clone
+//
+//  Created by faishal on 11/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
